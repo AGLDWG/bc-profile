c706eeb97df6207b3900d1028f70e20948ae3cde
Nicholas Car <nick@kurrawong.ai>
2025-11-17 07:45:12 +0100

    examples

---
+++ examples/validate.py
@@ -0,0 +1,19 @@
+import sys
+from pathlib import Path
+import re
+from pyshacl import validate
+from rdflib import Graph
+
+sg = Graph().parse("../validator.ttl")
+eg = Graph().parse("_dcceew.ttl")
+
+if len(sys.argv) == 1:
+    for f in sorted(list(Path(".").glob("*.ttl"))):
+        if re.match(r"^([0-9]{2}).*\.ttl$", str(f.name)):
+            dg = Graph().parse(f)
+            v = validate(dg + eg, shacl_graph=sg)
+            print(f"{f.name}: {v[0]}")
+else:
+    dg = Graph().parse(sys.argv[1])
+    v = validate(dg + eg, shacl_graph=sg)
+    print(v[0])
